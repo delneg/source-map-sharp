3bd864abb94787fba83272e0ae51fa868e052921
Denis Bobrov <delneg@yandex.ru>
2020-12-23 02:04:49 +0300

    Added ArraySet.fs

---
+++ ArraySet.fs
@@ -0,0 +1,32 @@
+module source_map_sharp.ArraySet
+
+open System.Collections.Generic
+
+type ArraySet<'T when 'T : equality>() as this =
+    member val _array = new ResizeArray<'T>()
+    member val _set = new Dictionary<'T,int>()
+    
+    member _.Size() = this._array.Count
+    member _.Has(aStr:'T) =
+        this._set.ContainsKey(aStr)
+    member _.indexOf(aStr: 'T) =
+        if this._set.ContainsKey(aStr) then Some this._set.[aStr] else None
+    member _.Add(aStr: 'T,aAllowDuplicates:bool) =
+        let isDuplicate = this._array.Contains aStr
+        let idx = this.Size()
+        if not isDuplicate || aAllowDuplicates then
+            this._array.Add(aStr)
+        if not isDuplicate then
+            this._set.[aStr] <- idx
+    member _.At(aIdx: int) =
+        if aIdx >= 0 && aIdx < this._array.Count then
+            Some this._array.[aIdx]
+        else None
+    member _.ToArray() = this._array
+    static member fromArray(aArray: 'T [], allowDuplicates) =
+        let s = new ArraySet<'T>()
+        for i in aArray do
+            s.Add(i,allowDuplicates)
+        s
+    
+    
