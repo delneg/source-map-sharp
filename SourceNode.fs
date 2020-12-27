e150f1f6e832dde6525c1696cc5c01ce58953d78
Denis Bobrov <delneg@yandex.ru>
2020-12-27 18:18:19 +0300

    Added Prepend, Walk, Join, ToString to SourceNode.fs

--- SourceNode.fs
+++ SourceNode.fs
@@ -5,27 +5,60 @@ open System.Text.RegularExpressions
 module SourceNode =
     let REGEX_NEWLINE = Regex @"(\r?\n)"
     let NEWLINE_CODE = 10
+
+type SourceMappingOriginal = {
+    line: int option
+    column: int option
+    Source: string option
+    Name: string option
+}
 type SourceChunk =
     | ChunkS of string
     | ChunkArrS of string[]
     | ChunkArrSN of SourceNode[]
 and SourceNodeChild = S of string | SN of SourceNode
 and SourceNode(?_line: int, ?_column: int, ?_source: string, ?_chunks: SourceChunk[], ?_name: string) as this =
-//    do (if _chunks.IsSome then _chunks.Value |> Array.iter this.Add else ())
+    do if _chunks.IsSome then
+            _chunks.Value |> Array.iter (fun x -> this.Add x |> ignore)
+        else ()
+
     member val line = _line
     member val column = _column
     member val source = _source
     member val name = _name
-    member val children = ResizeArray<SourceNodeChild>()
+    member val children = ResizeArray<SourceNodeChild>() with get,set
 
     
-    member _.Add(chunk: SourceChunk) =
+    member _.Add(chunk: SourceChunk) : SourceNode =
         match chunk with
         | ChunkS str -> this.children.Add(S str)
         | ChunkArrS arrs -> arrs |> Array.iter (fun x -> this.children.Add(S x))
         | ChunkArrSN arrsn -> arrsn |> Array.iter (fun x -> this.children.Add(SN x))
         this
+    member _.Prepend(chunk: SourceChunk) =
+        match chunk with
+        | ChunkS str -> this.children.Insert(0,S str)
+        | ChunkArrS arrs -> this.children.InsertRange(0, arrs |> Array.map S)
+        | ChunkArrSN arrsn -> this.children.InsertRange(0, arrsn |> Array.map SN)
+        this
     
+    member _.Walk(fn: string -> SourceMappingOriginal -> unit) =
+        for chunk in this.children do
+            match chunk with
+            | S str -> fn str {line = this.line;column=this.column;Source=this.source;Name=this.name}
+            | SN sn -> sn.Walk(fn)
     
+    member _.Join(sep: string) =
+        if this.children.Count > 0 then
+            let mutable newChildren = ResizeArray<SourceNodeChild>()
+            for i in [0..this.children.Count - 1] do
+                newChildren.Add(this.children.[i])
+                newChildren.Add(S sep)
+            newChildren.Add(this.children.[this.children.Count])
+            this.children <- newChildren
+        this
     
-
+    override _.ToString() =
+        let sb = System.Text.StringBuilder()
+        this.Walk(fun s _ -> sb.Append(s) |> ignore)
+        sb.ToString()
