9007e9e4bf144cf74c16bd3c9ceb6d336e28ce3c
Denis Bobrov <delneg@yandex.ru>
2020-12-27 19:23:13 +0300

    Added comments from js to SourceNode.fs

--- SourceNode.fs
+++ SourceNode.fs
@@ -4,7 +4,10 @@ open System.Text.RegularExpressions
 open System.Collections.Generic
 module SourceNode =
     let REGEX_NEWLINE = Regex @"(\r?\n)"
+    // Newline character code for charCodeAt() comparisons
     let NEWLINE_CODE = 10
+    
+    //We don't need `isSourceNode` since we have DU's
 
 type SourceMappingOriginal = {
     line: int option
@@ -17,6 +20,10 @@ type SourceChunk =
     | ChunkArrS of string[]
     | ChunkArrSN of SourceNode[]
 and SourceNodeChild = S of string | SN of SourceNode
+
+// SourceNodes provide a way to abstract over interpolating/concatenating
+// snippets of generated JavaScript source code while maintaining the line and
+// column information associated with the original source code.
 and SourceNode(?_line: int, ?_column: int, ?_source: string, ?_chunks: SourceChunk[], ?_name: string) as this =
     do if _chunks.IsSome then
             _chunks.Value |> Array.iter (fun x -> this.Add x |> ignore)
@@ -29,26 +36,31 @@ and SourceNode(?_line: int, ?_column: int, ?_source: string, ?_chunks: SourceChu
     member val children = ResizeArray<SourceNodeChild>() with get,set
     member val sourcesContents = Dictionary<string,string>()
 
-    
+    //  Add a chunk of generated JS to this source node.
     member _.Add(chunk: SourceChunk) : SourceNode =
         match chunk with
         | ChunkS str -> this.children.Add(S str)
         | ChunkArrS arrs -> arrs |> Array.iter (fun x -> this.children.Add(S x))
         | ChunkArrSN arrsn -> arrsn |> Array.iter (fun x -> this.children.Add(SN x))
         this
+    // Add a chunk of generated JS to the beginning of this source node.
     member _.Prepend(chunk: SourceChunk) =
         match chunk with
         | ChunkS str -> this.children.Insert(0,S str)
         | ChunkArrS arrs -> this.children.InsertRange(0, arrs |> Array.map S)
         | ChunkArrSN arrsn -> this.children.InsertRange(0, arrsn |> Array.map SN)
         this
-    
+    // Walk over the tree of JS snippets in this node and its children. The
+    // walking function is called once for each snippet of JS and is passed that
+    // snippet and the its original associated source's line/column location.
     member _.Walk(fn: string -> SourceMappingOriginal -> unit) =
         for chunk in this.children do
             match chunk with
             | S str -> fn str {line = this.line;column=this.column;Source=this.source;Name=this.name}
             | SN sn -> sn.Walk(fn)
     
+    //  Like `String.prototype.join` except for SourceNodes. Inserts `aStr` between
+    // each of `this.children`.
     member _.Join(sep: string) =
         if this.children.Count > 0 then
             let mutable newChildren = ResizeArray<SourceNodeChild>()
@@ -58,12 +70,15 @@ and SourceNode(?_line: int, ?_column: int, ?_source: string, ?_chunks: SourceChu
             newChildren.Add(this.children.[this.children.Count])
             this.children <- newChildren
         this
-    
+    //  Return the string representation of this source node. Walks over the tree
+    //  and concatenates all the various snippets together to one string.
     override _.ToString() =
         let sb = System.Text.StringBuilder()
         this.Walk(fun s _ -> sb.Append(s) |> ignore)
         sb.ToString()
 
+    // Call String.prototype.replace on the very right-most source snippet. Useful
+    //  for trimming whitespace from the end of a source node, etc.
     member _.ReplaceRight(pattern:string, replacement:string) =
         let lastChild = this.children |> Seq.tryLast
         match lastChild with
@@ -75,9 +90,13 @@ and SourceNode(?_line: int, ?_column: int, ?_source: string, ?_chunks: SourceChu
             this.children.Add("".Replace(pattern,replacement) |> S)
             this
     
+    // Set the source content for a source file. This will be added to the SourceMapGenerator
+    //  in the sourcesContent field.
     member _.SetSourceContent(file,content) =
         this.sourcesContents.[file] <- content
     
+    // Walk over the tree of SourceNodes. The walking function is called for each
+    //  source file content and is passed the filename and source content.
     member _.WalkSourceContents fn =
         for child in this.children do
             match child with
@@ -85,6 +104,8 @@ and SourceNode(?_line: int, ?_column: int, ?_source: string, ?_chunks: SourceChu
             | _ -> ()
         this.sourcesContents |> Seq.iter (fun kvp -> fn(kvp.Key,kvp.Value))
     
+    // Returns the string representation of this source node along with a source
+    // map.
     member _.ToStringWithSourceMap(skipValidation:bool option,file:string option,sourceRoot:string option) =
         let map = SourceMapGenerator(skipValidation,file,sourceRoot)
         let mutable generatedCode = System.Text.StringBuilder()
