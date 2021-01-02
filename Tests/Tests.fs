b0c481472cab1f117fda349286d726949a81a615
Denis Bobrov <delneg@yandex.ru>
2021-01-02 16:23:55 +0300

    All tests for SourceMapGenerator except not applicable & consumer-related

--- Tests/Tests.fs
+++ Tests/Tests.fs
@@ -848,3 +848,117 @@ module SourceMapGeneratorTests =
              sourceRoot=None
             }
         Assert.Equal (map.ToString(),JsonSerializer.Serialize(expected))
+    [<Fact>]
+    let ``test ignore duplicate mappings.`` () =
+        let map1 = SourceMapGenerator(file="min.js",sourceRoot="/the/root")
+        let map2 = SourceMapGenerator(file="min.js",sourceRoot="/the/root")
+        
+        let nullMapping1: MappingIndex = { line= 1; column= 0 }
+        let nullMapping2: MappingIndex = { line= 2; column= 2 }
+
+        map1.AddMapping(nullMapping1)
+        map1.AddMapping(nullMapping1)
+        
+        map2.AddMapping(nullMapping1)
+        
+        Assert.Equal (map1.ToString(),map2.ToString())
+        
+        map1.AddMapping(nullMapping2)
+        map1.AddMapping(nullMapping1)
+
+        map2.AddMapping(nullMapping2)
+        
+        Assert.Equal (map1.ToString(),map2.ToString())
+        
+        let srcMapping1Generated: MappingIndex = { line= 1; column= 0 }
+        let srcMapping1Original: MappingIndex = { line= 11; column= 0 }
+        let srcMapping1Source = "srcMapping1.js"
+        
+        let srcMapping2Generated: MappingIndex = { line= 2; column= 2 }
+        let srcMapping2Original: MappingIndex = { line= 11; column= 0 }
+        let srcMapping2Source = "srcMapping2.js"
+        
+        let map1 = SourceMapGenerator(file="min.js",sourceRoot="/the/root")
+        let map2 = SourceMapGenerator(file="min.js",sourceRoot="/the/root")
+        
+        map1.AddMapping(srcMapping1Generated,srcMapping1Original,srcMapping1Source)
+        map1.AddMapping(srcMapping1Generated,srcMapping1Original,srcMapping1Source)
+        
+        map2.AddMapping(srcMapping1Generated,srcMapping1Original,srcMapping1Source)
+        
+        Assert.Equal (map1.ToString(),map2.ToString())
+        
+        map1.AddMapping(srcMapping2Generated,srcMapping2Original,srcMapping2Source)
+        map1.AddMapping(srcMapping1Generated,srcMapping1Original,srcMapping1Source)
+        
+        map2.AddMapping(srcMapping2Generated,srcMapping2Original,srcMapping2Source)
+        
+        Assert.Equal (map1.ToString(),map2.ToString())
+        
+        
+        let fullMapping1Generated: MappingIndex = { line= 1; column= 0 }
+        let fullMapping1Original: MappingIndex = { line= 11; column= 0 }
+        let fullMapping1Source = "fullMapping1.js"
+        let fullMapping1Name = "fullMapping1"
+        
+        let fullMapping2Generated: MappingIndex = { line= 2; column= 2 }
+        let fullMapping2Original: MappingIndex = { line= 11; column= 0 }
+        let fullMapping2Source = "fullMapping2.js"
+        let fullMapping2Name = "fullMapping2"
+        
+        let map1 = SourceMapGenerator(file="min.js",sourceRoot="/the/root")
+        let map2 = SourceMapGenerator(file="min.js",sourceRoot="/the/root")
+        
+        map1.AddMapping(fullMapping1Generated,fullMapping1Original,fullMapping1Source,fullMapping1Name)
+        map1.AddMapping(fullMapping1Generated,fullMapping1Original,fullMapping1Source,fullMapping1Name)
+        
+        map2.AddMapping(fullMapping1Generated,fullMapping1Original,fullMapping1Source,fullMapping1Name)
+        
+        Assert.Equal (map1.ToString(),map2.ToString())
+        
+        map1.AddMapping(fullMapping2Generated,fullMapping2Original,fullMapping2Source,fullMapping2Name)
+        map1.AddMapping(fullMapping1Generated,fullMapping1Original,fullMapping1Source,fullMapping1Name)
+        
+        map2.AddMapping(fullMapping2Generated,fullMapping2Original,fullMapping2Source,fullMapping2Name)
+        Assert.Equal (map1.ToString(),map2.ToString())
+        
+        
+    [<Fact>]
+    let ``test github issue #72, check for duplicate names or sources`` () =
+        let map = SourceMapGenerator(file="test.js")
+        let generated1: MappingIndex = { line= 1; column= 1 }
+        let original1: MappingIndex = { line= 2; column= 2 }
+        map.AddMapping(generated1, original1, "a.js", "foo")
+
+        let generated2: MappingIndex = { line= 3; column= 3 }
+        let original2: MappingIndex = { line= 4; column= 4 }
+        map.AddMapping(generated2, original2, "a.js", "foo")
+
+        let expected = {
+             version=3
+             file="test.js"|>Some
+             sources= seq {"a.js"}
+             names= seq {"foo"}
+             mappings="CACEA;;GAEEA"
+             sourcesContent=None
+             sourceRoot=None
+            }
+        Assert.Equal (map.ToString(),JsonSerializer.Serialize(expected))
+        
+    [<Fact>]
+    let ``test setting sourcesContent to null when already null`` () =
+        let map = SourceMapGenerator(file="foo.js")
+        map.SetSourceContent("bar.js",None)
+    
+    [<Fact>]
+    let ``test numeric names #231`` () =
+        let map = SourceMapGenerator()
+        let generated: MappingIndex = {line=1;column=10}
+        let original: MappingIndex = {line=1;column=10}
+        // type safe names! can't pass number
+        map.AddMapping(generated,original, source="a.js",name="8")
+        
+        let m = map.toJSON()
+        Assert.Equal(m.names |> Seq.length,1)
+        Assert.Equal(m.names |> Seq.head,"8")
+       
