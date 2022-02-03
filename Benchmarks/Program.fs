89f3a0c3eca0d9decd97e2b471ef419d9822b7e6
Denis Bobrov <delneg@yandex.ru>
2022-02-03 20:41:12 +0300

    Benchmark setup project

--- Benchmarks/Program.fs
+++ Benchmarks/Program.fs
@@ -1,4 +1,28 @@
+open System.Collections.Generic
+open BenchmarkDotNet.Attributes
+open BenchmarkDotNet.Running
+open Bogus
+let fake = Faker()
 
+[<PlainExporter; MemoryDiagnoser>]
+type CustomArray() =
+    [<DefaultValue; Params(10000)>]
+    val mutable public N : int
+    
+    let mutable nums = Array.empty
+    
+   
+    [<GlobalSetup>]
+    member this.Setup() =
+        let first = [|for _ in 1..this.N do fake.Random.Long()|]
+        nums <-  first
+        
+    [<Benchmark>]
+    member _.ArrayFilter() = Array.filter (fun x -> x % 2L = 0L) nums
+  
+    
 
-// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+[<EntryPoint>]
+let main argv =
+    let arrays = BenchmarkRunner.Run<CustomArray>()
+    0 // return an integer exit code
